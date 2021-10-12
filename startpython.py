3028d982d4260b86e555cbf35dcc2a80ca513279
AiRikux <nabilahqanuwar@gmail.com>
2021-10-12 21:59:17 +0900

    initial commit

---
+++ startpython.py
@@ -0,0 +1,31 @@
+# We are still introduced to Python
+
+# basic python
+
+a = 3 + 3
+print(a)
+
+a = int(input("first number: "))
+b = int(input("second number: "))
+total = a + b
+print("Total = ", total)
+
+# branching
+kkey = input("Password: ")
+
+if kkey == 'test123':
+	print("Login successful")
+else:
+	print("Login failed")
+
+# repeating
+for x in range(5):
+	print("number = ", x)
+
+counter = 1
+maxy = 3
+
+while counter < maxy:
+	print(counter)
+	counter = counter + 1
+
